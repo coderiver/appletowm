e50d4ad8431cbb2421411f2bb40ed996657fcb1e
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2016-02-15 18:14:43 +0200

    basic styles and header added

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,9 +6,9 @@ gulp.task('watch', [
     'sprite:watch',
     'sass:watch',
     'copy:watch',
-    'html:watch',
     'font:watch',
-    'js:watch'
+    'js:watch',
+    'jade:watch'
 ]);
 
 
@@ -16,4 +16,4 @@ gulp.task('delete', function (cb) {
     rimraf('./'+config.dest.root, cb);
 });
 gulp.task('default', ['server', 'watch'], function() {});
-gulp.task('build', ['html','font','sprite','copy','js','sass'], function() {});
+gulp.task('build', ['jade','font','sprite','copy','js','sass'], function() {});
