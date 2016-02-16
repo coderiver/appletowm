43cc8d1e52634f4db2f06049781c4bb512a0d9dd
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2016-02-16 19:57:43 +0200

    desktop added

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include lib/validator.js
+//=include lib/mask.js
 //=include common.js
--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,62 @@
 $(document).ready(function() {
 
-	// go js
-
+	// mask
+	$('.js-phone').mask("+7 999-999-99-99");
+	// form validation
+	(function() {
+		var popup   = $('.js-popup'),
+			thanks  = $('.js-thanks-popup'),
+			body    = $('body');
+		// welcome
+		$.validate({
+			form: '#popup-form',
+			onSuccess: function() {
+				post_data = {
+					'name': $('#popup-form input[name=name]').val(),
+					'tel': $('#popup-form input[name=tel]').val()
+				};
+				// Ajax post data to server
+				$.post('send.php', post_data, function(response) {
+					if (response.type == 'error') {
+						console.log('error');
+					}
+					//else {
+					//	// reset values in all input fields
+					//	popup.removeClass('is-open');
+					//	thanks.addClass('is-open');
+					//	$('#popup-form').get(0).reset();
+					//	setTimeout(function() {
+					//		thanks.removeClass('is-open');
+					//	}, 2000);
+					//}
+				}, 'json');
+				return false;
+			}
+		});
+		// footer
+		$.validate({
+			form : '#form',
+			onSuccess: function() {
+				post_data = {
+					'name': $('#form input[name=name]').val(),
+					'surname': $('#form input[name=surname]').val(),
+					'tel': $('#form input[name=tel]').val(),
+					'email': $('#form input[name=email]').val(),
+					'comment': $('#form textarea[name=comment]').val()
+				};
+				// Ajax post data to server
+				$.post('send.php', post_data, function(response) {
+					if (response.type == 'error') {}
+					//else {
+					//	thanks.addClass(500);
+					//	$('#form').get(0).reset();
+					//	setTimeout(function() {
+					//		thanks.removeClass('is-open');
+					//	}, 2000);
+					//}
+				}, 'json');
+				return false;
+			}
+		});
+	}());
 });
