cb3868d30cb96caf6fdb57946577b5db32ec0ef7
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2016-02-17 14:43:52 +0200

    fixes

--- src/js/common.js
+++ src/js/common.js
@@ -59,4 +59,46 @@ $(document).ready(function() {
 			}
 		});
 	}());
+
+	// callback-btn
+
+	//function FixedOnCenter(sector) {
+	//	this.sector = sector;
+	//
+	//	return function() {
+	//
+	//		this.sayHi =  function() {
+	//			console.log(this);
+	//		}
+	//
+	//
+	//	}
+	//	return this;
+	//
+	//}
+	////
+	//var fixedBtn = new FixedOnCenter($('.js-fixed-btn'));
+	//fixedBtn();
+	//console.log(fixedBtn.sayHi())
+
+	var center = ($('.js-fixed-btn').offset().top + 60) - ($(window).outerHeight() / 2);
+	var center2 = ($('.js-section-center').offset().top + $('.js-section-center').outerHeight() / 2) - ($(window).outerHeight() / 2);
+	var endPoint = $('.js-section-center').offset().top + ($('.js-section-center').outerHeight() / 2);
+	function makeCenter() {
+		if($(window).scrollTop() >= center) {
+			$('.js-fixed-btn').addClass('is-fixed');
+			$('.js-fixed-btn').css('top', '50%');
+
+		} else {
+			$('.js-fixed-btn').removeClass('is-fixed');
+		}
+		if($(window).scrollTop() >= center2) {
+			$('.js-fixed-btn').removeClass('is-fixed');
+			$('.js-fixed-btn').css('top', center2);
+		}
+	}
+	$(window).on('scroll', makeCenter);
+
+
+	//console.log(btn.sector);
 });
